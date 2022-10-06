ea91bb6e1b82b54df316eb9cd845803e81606fb3
Predox <115118401+Predox@users.noreply.github.com>
2022-10-05 21:46:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,50 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+var i = setInterval(function () {
+  clearInterval(i);
+
+  document.getElementById("loading").style.display = "none";
+  document.getElementById("content").style.display = "block";
+ 
+}, 1000);
+
+
+
+function colorirImgs(){
+  let colorir = document.querySelectorAll('.container-align');
+  colorir[0].setAttribute('class', 'container-align-color')
+  colorir[1].setAttribute('class', 'container-align-color')
+  colorir[2].setAttribute('class', 'container-align-color')
+  colorir[3].setAttribute('class', 'container-align-color')
+  colorir[4].setAttribute('class', 'container-align-color')
+  colorir[5].setAttribute('class', 'container-align-color')
+  colorir[6].setAttribute('class', 'container-align-color')
+  colorir[7].setAttribute('class', 'container-align-color')
+  colorir[8].setAttribute('class', 'container-align-color')
+  colorir[9].setAttribute('class', 'container-align-color')
+  colorir[10].setAttribute('class', 'container-align-color')
+  colorir[11].setAttribute('class', 'container-align-color')
+  colorir[12].setAttribute('class', 'container-align-color')
+  colorir[13].setAttribute('class', 'container-align-color')
+}
+
+function descolorirImgs(){
+  let colorir = document.querySelectorAll('.container-align-color');
+  colorir[0].setAttribute('class', 'container-align')
+  colorir[1].setAttribute('class', 'container-align')
+  colorir[2].setAttribute('class', 'container-align')
+  colorir[3].setAttribute('class', 'container-align')
+  colorir[4].setAttribute('class', 'container-align')
+  colorir[5].setAttribute('class', 'container-align')
+  colorir[6].setAttribute('class', 'container-align')
+  colorir[7].setAttribute('class', 'container-align')
+  colorir[8].setAttribute('class', 'container-align')
+  colorir[9].setAttribute('class', 'container-align')
+  colorir[10].setAttribute('class', 'container-align')
+  colorir[11].setAttribute('class', 'container-align')
+  colorir[12].setAttribute('class', 'container-align')
+  colorir[13].setAttribute('class', 'container-align')
+}
+
