6a925cf52b78ef8047fd0c3f5551916170bb3703
Predox <115118401+Predox@users.noreply.github.com>
2022-10-05 21:47:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ var i = setInterval(function () {
   document.getElementById("loading").style.display = "none";
   document.getElementById("content").style.display = "block";
  
-}, 1000);
+}, 8000);
 
 
 
